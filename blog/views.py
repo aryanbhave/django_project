76aba512dcb4c5ce18b58314e3bbd91930799e45
Aryan Bhave <46799871+aryanbhave@users.noreply.github.com>
2022-11-07 00:36:36 -0800

    Add files via upload
    added table view to home page and edited navbar

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from users.models import referrer
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': Post.objects.all()
+        'posts': Post.objects.all(),
+        'query_results' : referrer.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
