2a78f79040117b028d948716f7d4a1e60bc4f96b
Aryan Bhave <46799871+aryanbhave@users.noreply.github.com>
2022-11-08 02:01:52 -0800

    Add files via upload
    updated profile page

--- users/views.py
+++ users/views.py
@@ -34,18 +34,50 @@ def register(request):
 def profile(request):
     if request.method == "POST":
         #Remove user from referrer database and auth users as well.
-        query = request.POST.get('usernameTBD', None)
-        print(query)
+        firstNameQuery = request.POST.get('changeFirst', None)
+        lastNameQuery = request.POST.get('changeSecond', None)
+        emailQuery = request.POST.get('changeEmail', None)
+        usernamequery = request.POST.get('usernameTBD', None)
 
         if request.user.is_authenticated:
-            username = request.user.username
-            if username == query:
-                referrer.objects.all().filter(username=query).delete()
-                User.objects.all().filter(username=query).delete()
+            usernameReq = request.user.username #Originally called username
+            if usernameReq == usernamequery:
+                referrer.objects.all().filter(username=usernameReq).delete()
+                User.objects.all().filter(username=usernameReq).delete()
                 messages.success(request, f'Your account has been successfully deleted. We are sorry to see you go!')
                 return redirect('logout')
+            elif firstNameQuery:
+                referrer.objects.all().filter(username=usernameReq).update(firstName=firstNameQuery)
+                User.objects.all().filter(username=usernameReq).update(first_name=firstNameQuery)
+                response = 'Your first name has been changed to ' + str(firstNameQuery)
+                messages.success(request, response)
+                return render(request, 'users/profile.html')
+            elif lastNameQuery:
+                referrer.objects.all().filter(username=usernameReq).update(lastName=lastNameQuery)
+                User.objects.all().filter(username=usernameReq).update(last_name=lastNameQuery)
+                response = 'Your last name has been changed to ' + str(lastNameQuery)
+                messages.success(request, response)
+                return render(request, 'users/profile.html')
+            elif emailQuery:
+                if '@' in emailQuery:
+                    referrer.objects.all().filter(username=usernameReq).update(email=emailQuery)
+                    User.objects.all().filter(username=usernameReq).update(email=emailQuery)
+                    response = 'Your email has been changed to ' + str(emailQuery)
+                    messages.success(request, response)
+                    return render(request, 'users/profile.html')
+                else:
+                    messages.info(request, 'Please type a valid email address.')
+                    return render(request, 'users/profile.html')
             else:
-                messages.info(request, 'The username was incorrect.')
+                messages.info(request, 'There was a problem while managing your profile. Please retry.')
                 return render(request, 'users/profile.html')
     else:  
         return render(request, 'users/profile.html')
+
+
+# def changeData(request, username, fieldTBU, queryInput):
+#     referrer.objects.all().filter(username=username).update(fieldTBU=queryInput)
+#     User.objects.all().filter(username=username).update(fieldTBU=queryInput)
+#     response = 'Your first name has been changed to ' + str(queryInput)
+#     messages.success(request, response)
+#     return render(request, 'users/profile.html')
