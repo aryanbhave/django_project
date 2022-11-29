a0fc083cbcad85947691c38747d340dd91b75e79
Aryan Bhave <46799871+aryanbhave@users.noreply.github.com>
2022-11-28 16:54:00 -0800

    Add files via upload
    text changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
-import users
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('referers/', views.referers, name='blog-referers'),
 ]
