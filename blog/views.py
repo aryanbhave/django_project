45e4f7146da5d76c5165337fcf27c4ffdc9f0a00
Aryan Bhave <46799871+aryanbhave@users.noreply.github.com>
2022-11-28 17:19:21 -0800

    Add files via upload
    uploaded wrong files to blog

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from users.models import referrer
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your views here.
 def home(request):
@@ -11,12 +13,21 @@ def home(request):
             'title': "Home",
             'query_results' : referrer.objects.all().filter(company=query).order_by('firstName') | referrer.objects.all().filter(firstName=query).order_by('lastName') | referrer.objects.all().filter(lastName=query).order_by('firstName')
         }
-        return render(request, 'blog/home.html', context)
+        return render(request, 'blog/home1.html', context)
     else:
         context = {
             'query_results' : referrer.objects.all().order_by('company')
         }
-        return render(request, 'blog/home.html', context)
+        return render(request, 'blog/home1.html', context)
+
+def referers(request):
+    context={
+        'list':referrer.objects.all()
+    }
+    if request.user.is_authenticated:
+        return render(request,'blog/referers.html',context)
+    else:
+        return render(request,'login1.html') 
 
 
 def about(request):
