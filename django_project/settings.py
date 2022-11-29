d8823575fb4d62bf4f5bfb828c3495cfbc88eab9
Sshaonak P Galvankar <shaunakprasad@icloud.com>
2022-11-29 15:58:27 -0800

    added allowed host aryans ec2

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ivh0tn(jhiw42(1+$rbnm5eun**g$-pk9#&zfa26)fpl(07o3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.202.117.75','127.0.0.1']
+ALLOWED_HOSTS = ['54.202.117.75','127.0.0.1','35.90.188.179']
 
 
 # Application definition
